7577717716e346d38bc49fa408fabc880e6793e6
dtroxx <dtroxx@gmail.com>
2018-02-21 11:03:20 -0800

    updates

--- app-promise.js
+++ app-promise.js
@@ -18,11 +18,25 @@ const argv = yargs.options({
   var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address='${encodedAddress}`;
 
   axios.get(geocodeUrl).then((response) => {
-    console.log(response.data);
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Unable to find that address.');
+    }
+    //console.log(response);
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/81607717edaaa3e6b5ab4d644e5bc3d8/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+  }).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
   }).catch((error) => {
-    //console.log(error);
+    console.log(error);
     if (error.code === 'ENOTFOUND') {
       console.log('Unable to connect to API servers.');
+    } else {
+      console.log(error.message);
     }
-  }) ;
+  });
   
